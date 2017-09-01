71a5e331ace7e0450b1b9cc63b15e2464283c5d9
gvaccari <tianxiang.vaccari@gmail.com>
2017-09-01 20:21:25 +0200

    First commit

---
+++ src/main/java/com/vibridi/rblock/Main.java
@@ -0,0 +1,7 @@
+package com.vibridi.rblock;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/vibridi/rblock/MainTest.java
@@ -0,0 +1,12 @@
+package com.vibridi.rblock;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+    public void testApp() {
+        // TODO auto-generated code
+    }
+}
